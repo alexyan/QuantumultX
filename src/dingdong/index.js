22eeb00b352e7761f10e00d92336857e8fa6674b
alexyan <5918760@gmail.com>
2022-03-22 18:09:38 +0800

    WIP:

--- src/dingdong/index.js
+++ src/dingdong/index.js
@@ -1,3 +1,5 @@
 // ^https:\/\/maicai\.api\.ddxq\.mobi\/order\/logistics url script-response-body https://raw.githubusercontent.com/alexyan/QuantumultX/webpack/build/dingdong.bundle.js
 
-$notify('叮咚', $request.url, $response.body)
+// $notify('叮咚', $request.url, $response.body)
+
+$notify('叮咚')
